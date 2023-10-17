b8064a4bfb04384778a44bb5d2e7f51019c6be34
edkbil <edkbil@ukr.net>
2023-10-17 17:00:35 +0300

    main block
    додано головний блок і список нижче

--- src/main.js
+++ src/main.js
@@ -1,1 +1,23 @@
 import "./styles/all.scss";
+
+const body = document.querySelector("main");
+const list = document.querySelector(".list");
+
+function mainRun() {
+  body.classList.add("normal");
+}
+function listRun() {
+  list.classList.add("normal");
+}
+setTimeout(mainRun, 800);
+
+let scrollPos = 0;
+function checkPosition() {
+  let windowY = window.scrollY;
+  if (windowY > scrollPos) {
+    setTimeout(listRun, 200);
+  }
+  scrollPos = windowY;
+}
+
+window.addEventListener("scroll", checkPosition);
